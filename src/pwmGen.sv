13afd9a5d40e1a826c5000e06cec9811e3c38971
Loshanan <loshanan221199@gmail.com>
2024-10-04 16:01:43 +0530

    warnings corrected

--- src/pwmGen.sv
+++ src/pwmGen.sv
@@ -1,6 +1,6 @@
 module pwmGen #(
-    parameter CLK_PERIOD;     
-    parameter DATA_WIDTH,
+    parameter CLK_PERIOD,     
+    parameter DATA_WIDTH
 ) (
     input clk,
     input logic [DATA_WIDTH - 1:0] pwmPeriod,   // in micro s
@@ -8,20 +8,30 @@ module pwmGen #(
     output logic pwm
 );
 
-    localparam pwmPeriod_ns = pwmPeriod * 1000;         // in ns
-    localparam pwmDutyCycle_ns = pwmDutyCycle * 1000;   // in ns
+    // localparam pwmPeriod_ns = pwmPeriod * 1000;         // in ns
+    // localparam pwmDutyCycle_ns = pwmDutyCycle * 1000;   // in ns
+
+    logic [DATA_WIDTH - 1:0] pwmPeriod_ns;
+    logic [DATA_WIDTH - 1:0] pwmDutyCycle_ns;
     
-    assign pwm = '0;
     logic [DATA_WIDTH - 1:0] counter;
 
-    assign counter = '0;
+    always_comb begin
+        pwmPeriod_ns = (pwmPeriod * 1000) / CLK_PERIOD;
+        pwmDutyCycle_ns = (pwmDutyCycle * 1000) / CLK_PERIOD;
+    end
+
+    initial begin
+        counter = '0;
+        pwm = '0;
+    end
 
     always_ff @( posedge clk ) begin
-        if (counter < pwmPeriod_ns ) counter <= counter + CLK_PERIOD;
+        if (counter < pwmPeriod_ns ) counter <= counter + 1;
         else counter <= 0;
     end
 
-    always_ff @( posedge ) begin
+    always_ff @( posedge clk ) begin
         if (counter < pwmDutyCycle_ns) pwm <= 1;
         else pwm <= 0;
     end
