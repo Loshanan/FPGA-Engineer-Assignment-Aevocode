2ba709145e5432d5e865f3716f62d5a330b7331e
Loshanan <loshanan221199@gmail.com>
2024-10-05 11:59:24 +0530

    AXI slave module fixed and testbench added

--- src/main.sv
+++ src/main.sv
@@ -5,17 +5,28 @@ module main #(
     parameter ADDR_WIDTH = 2,   // for those 3 registers
     parameter REG_UPDATE_FREQ = 10   // speed register update frequency
 ) (
-    input logic clk,
+    input logic clk, rst,
 
-    input logic [ADDR_WIDTH - 1:0] writeAddress,
-    input logic [DATA_WIDTH - 1:0] writeData,
-    output logic writeResponse,
+    // axi write signals
+    input logic awvalid,        // write address valid
+    output logic awready,       // slave ready to accept write adress
+    input logic [ADDR_WIDTH - 1:0] awaddr, //write address
+    input logic [DATA_WIDTH - 1:0] wdata, //write data
+    input logic wvalid,         // write data valid
+    output logic wready,        // slave ready to accept data
+    output logic wresp,         // write response
 
-    input logic [ADDR_WIDTH - 1:0] readAddress,
-    output logic [DATA_WIDTH - 1:0] readData,
+    // axi read signal
+    input logic arvalid,        // valid read address presents
+    output logic arready,       // slave ready to accept read address
+    input logic [ADDR_WIDTH - 1:0] araddr, //read address
+    output logic [DATA_WIDTH - 1:0] rData, // read data
+    output logic rvalid,        // valid read response is present
 
+    // encoder pulse output from the motor
     input logic pulseData,
 
+    // desired pwm output
     output logic PWM
 );
     
@@ -23,10 +34,10 @@ module main #(
     logic [31:0] reg2;
     logic [31:0] reg3;
     
-    axiSlave #(DATA_WIDTH, ADDR_WIDTH) slave(clk, writeAddress, writeData, writeResponse, readAddress, readData, reg1, reg2, reg3);
+    axiSlave #(DATA_WIDTH, ADDR_WIDTH) axislave(.*);
     
-    pwmGen #(CLK_PERIOD, DATA_WIDTH) pwmgen(clk, reg2, reg3, PWM);
+    pwmGen #(CLK_PERIOD, DATA_WIDTH) pwmgen(clk, rst, reg2, reg3, PWM);
 
-    fCounter #(CLK_PERIOD, PPR, REG_UPDATE_FREQ) rpmcounter(clk, pulseData, reg1);
+    fCounter #(CLK_PERIOD, PPR, REG_UPDATE_FREQ) rpmcounter(clk, rst, pulseData, reg1);
 
 endmodule
